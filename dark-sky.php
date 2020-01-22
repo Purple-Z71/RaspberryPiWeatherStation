25b6570c130374ac071cb804db3621d11e0e4c6b
Purple-Z71 <60164632+Purple-Z71@users.noreply.github.com>
2020-01-21 20:03:46 -0600

    Create dark-sky.php

---
+++ dark-sky.php
@@ -0,0 +1,47 @@
+<?php
+
+class Forecast
+{
+    const API_ENDPOINT = 'https://api.darksky.net/forecast/';
+    private $api_key;
+    public function __construct($api_key)
+    {
+        $this->api_key = $api_key;
+    }
+    private function request($latitude, $longitude, $time = null, $options = array())
+    {
+        $request_url = self::API_ENDPOINT
+            . $this->api_key
+            . '/'
+            . $latitude
+            . ','
+            . $longitude
+            . ((is_null($time)) ? '' : ','. $time);
+        if (!empty($options)) {
+            $request_url .= '?'. http_build_query($options);
+        }
+        
+        $response = json_decode(file_get_contents($request_url));
+        $response->headers = $http_response_header;
+        return $response;
+    }
+    public function get($latitude, $longitude, $time = null, $options = array())
+    {
+        return $this->request($latitude, $longitude, $time, $options);
+    }
+}
+
+// Add your API key here
+$forecast = new Forecast("https://api.darksky.net/forecast/cda5c006ac99037fd8c3017443f238a9");
+
+// Set timezone
+date_default_timezone_set('America/Chicago');
+$date = new DateTime();
+
+// Get the forecast at a given time - default coordinates Times Square
+$data = json_encode($forecast->get('38.6270, 90.1994'));
+echo header("Access-Control-Allow-Origin: *");
+echo $data;
+
+
+?>
